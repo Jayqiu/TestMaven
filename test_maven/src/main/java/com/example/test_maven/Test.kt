1ce4b331dd3e06f5d1a9f5cef3d7a72cb216fb8d
jayqiu <jay.qiu087@gmail.com>
2021-12-23 14:18:56 +0800

    publish 1.0.2

---
+++ test_maven/src/main/java/com/example/test_maven/Test.kt
@@ -0,0 +1,12 @@
+package com.example.test_maven
+
+/**
+ * @author: JayQiu
+ * @date: 2021/12/23
+ * @description:
+ */
+public class Test {
+    companion object{
+        const val time=1000
+    }
+}
